64ec9794d5e3e398722c9f1c9bd28bcb50155e33
yyrichy <ryvapor@gmail.com>
2025-12-04 02:28:21 -0500

    init draft

---
+++ apps/backend/index.ts
@@ -0,0 +1,12 @@
+import { Server } from '@hocuspocus/server'
+
+const server = new Server({
+  port: 1234,
+  onConnect: async (data) => {
+    console.log(`Connected: ${data.documentName}`)
+  },
+})
+
+server.listen().then(() => {
+  console.log('Hocuspocus listening on port 1234')
+})
